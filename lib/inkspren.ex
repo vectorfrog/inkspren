df9829a69f0105f316763badd566751f1aaeefa1
Kevin Wilson <kevinparrywilson@gmail.com>
2020-12-16 15:58:42 -0700

    first commit

---
+++ lib/inkspren.ex
@@ -0,0 +1,18 @@
+defmodule Inkspren do
+  @moduledoc """
+  Documentation for `Inkspren`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Inkspren.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/inkspren_test.exs
@@ -0,0 +1,8 @@
+defmodule InksprenTest do
+  use ExUnit.Case
+  doctest Inkspren
+
+  test "greets the world" do
+    assert Inkspren.hello() == :world
+  end
+end
